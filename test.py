0d4b27307a8ba49c527d343921fa4dc6bf507cc7
ivopbernardo <ivopbernardo@gmail.com>
2023-03-01 17:34:47 +0000

    afinal sou simpático e cumprimento as pessoas

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def ola_mundo(nome):
-    saudacao = 'Sou a/o {} e já sou capaz de não fazer notebooks v_finalissima'.format(nome)
+    saudacao = 'Olá a todos! Sou a/o {} e já sou capaz de não fazer notebooks v_finalissima'.format(nome)
     return saudacao
 
 print(ola_mundo('Ivo'))
