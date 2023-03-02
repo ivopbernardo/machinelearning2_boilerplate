96d4568ffe84142460d553713726e644fa2d675e
Fernando <fernando_cordeiro@live.com>
2023-03-02 17:49:12 +0000

    Add type hints and docstrings

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-def hello(name):
+def hello(name: str) -> str:
+    """Returns a greeting using someone's name"""
     greeting = 'Olá a todos! Sou a/o {} e já sou capaz de não fazer notebooks v_finalissima'.format(name)
     return greeting
 
